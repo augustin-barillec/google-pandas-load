3b5fc29e437d8284e1db418af7c074e1461a1c4c
abarillec <augustin.barillec@ysance.com>
2019-04-11 21:44:00 +0200

    changed version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = '2019, Augustin Barillec'
 author = 'Augustin Barillec'
 
 # The short X.Y version
-version = '0.1'
+version = '1.0'
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '1.0.0'
 
 
 # -- General configuration ---------------------------------------------------
