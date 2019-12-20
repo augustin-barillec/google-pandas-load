6ab0f9cfa3a79ddf0e79f642c325e61216fbbf42
abarillec <augustin.barillec@ysance.com>
2019-12-20 12:03:16 +0100

    typo version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='google-pandas-load',
-    version='2.0.3',
+    version='2.0.1',
     author='Augustin Barillec',
     author_email='augustin.barillec@ysance.com',
     description=(
