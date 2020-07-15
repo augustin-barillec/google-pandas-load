59d59490aa23180b66441e8cd349219c94f0a867
augustin <augustin.barillec@gmail.com>
2020-07-15 18:47:56 +0200

    2.0.1 => 3.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='google-pandas-load',
-    version='2.0.1',
+    version='3.0',
     author='Augustin Barillec',
     author_email='augustin.barillec@ysance.com',
     description=(
