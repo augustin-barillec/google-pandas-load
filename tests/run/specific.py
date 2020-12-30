dfc3e9acb1c0e16838425b9481c017f0e688b531
augustinbarillec <augustin.barillec@easyence.com>
2020-12-30 18:26:27 +0100

    upgraded python-bq => 2.*

--- tests/run/specific.py
+++ tests/run/specific.py
@@ -1,7 +1,6 @@
 import unittest
-from tests.unit.test_getters import CallGetters
+from tests.unit.test_data_delivery import DataDeliveryTest
 
 suite = unittest.TestSuite()
-suite.addTest(CallGetters('test_call_loader_getters'))
-suite.addTest(CallGetters('test_call_loader_quick_setup_getters'))
+suite.addTest(DataDeliveryTest('test_download_upload'))
 unittest.TextTestRunner(verbosity=2).run(suite)
