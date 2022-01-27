cd3c2b98b8aab3f3707b2ca5d9daa7eaa552d06b
augustinbarillec <augustin.barillec@easyence.com>
2022-01-27 16:57:37 +0100

    symplify dev_setup.sh

--- dev_setup.sh
+++ dev_setup.sh
@@ -4,10 +4,6 @@ echo "Starting update..."
 apt-get update
 echo "Ended update"
 
-echo "Installing python3-venv..."
-apt-get install python3-venv
-echo "Installed python3-venv"
-
 echo "Installing pandoc..."
 apt-get install pandoc
 echo "Installed pandoc"
