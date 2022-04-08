fd0bbb4072ff03009bbd77bf43d3702c131b7e7e
augustinbarillec <augustin.barillec@easyence.com>
2022-04-08 17:51:33 +0200

    wip

--- setup.py
+++ setup.py
@@ -20,13 +20,13 @@ setup(
     install_requires=REQUIREMENTS,
     packages=find_namespace_packages(include=['google_pandas_load*']),
     classifiers=[
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "License :: OSI Approved :: MIT License",
-        "OPERATING SYSTEM :: POSIX :: LINUX"],
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux'],
     project_urls={
         'Documentation':
             'https://google-pandas-load.readthedocs.io/en/latest/',
